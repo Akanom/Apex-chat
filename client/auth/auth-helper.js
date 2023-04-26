e1692137ef1241dfb728fc6f7d54108fa2235e33
Oluwajuwon Akanbi <omokoreakanbi@gmail.com>
2023-04-26 06:41:19 +0200

    sc50001A3.2.1-260423FA

--- client/auth/auth-helper.js
+++ client/auth/auth-helper.js
@@ -1,34 +1,31 @@
-import { signout } from './api-auth.js'
+import { signout } from "./api-auth.js";
 //Adding auth to the frontend
 
-//if user successfully sign-in, save jwt credentials
-authenticate(jwt, bc);
-{
-  if (typeof window !== "undefined")
-    //window if sucessfully defined
-    localStorage.setItem("jwt", JSON.stringify(jwt)); //session stored in localStorage
-  bc();
-}
+const auth = {
+  //if user successfully sign-in, save jwt credentials
+  authenticate(jwt, bc) {
+    if (typeof window !== "undefined")
+      //window if sucessfully defined
+      localStorage.setItem("jwt", JSON.stringify(jwt)); //session stored in localStorage
+    bc();
+  },
 
-//retreive the stored credentials to check that the current user is signed in
-isAuthenticated();
-{
-  if (typeof window == "undefined") return false;
-  if (localStorage.getItem("jwt"))
-    return JSON.parse(localStorage.getItem("jwt"));
-  else return false;
-}
+  //retreive the stored credentials to check that the current user is signed in
+  isAuthenticated() {
+    if (typeof window == "undefined") return false;
+    if (localStorage.getItem("jwt"))
+      return JSON.parse(localStorage.getItem("jwt"));
+    else return false;
+  },
 
+  //Deleting credentials when a user sign-out
+  clearJWT(bc) {
+    if (typeof window !== "undefined") localStorage.removeItem("jwt");
+    bc();
+    signout().then((data) => {
+      document.cookie = "t=; expires=Wed, 31 Dec 1969 00:00:00 UTC; path=/;";
+    });
+  },
+};
 
-//Deleting credentials when a user sign-out
-clearJWt(bc)
-{
-if (typeof window !=="undefined")
-    localStorage.removeItem("jwt")
-    bc()
-    signout().then((data)=>{
-        document.cookie="t=; expires=Wed, 31 Dec 1969 00:00:00 UTC; path=/;"
-    })
-}
-
-export default auth
+export default auth;
