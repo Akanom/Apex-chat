6513fcb599638041d2a26d7059667b6d9b9ea9e9
Oluwajuwon Akanbi <omokoreakanbi@gmail.com>
2023-05-03 22:38:53 +0200

    sc50001A3.1-030523P

---
+++ client/user/Profile.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState } from "react";
+
+const Profile = () => {
+  const [user, setUser] = useState({});
+  const [redirectToSignIn, setRedirectToSignIn] = useState(false);
+  return <div>Profile</div>;
+};
+
+export default Profile;
