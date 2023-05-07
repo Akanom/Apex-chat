5185cb5ff8bff0903e71d58a2b79900eb880e7f5
Oluwajuwon Akanbi <omokoreakanbi@gmail.com>
2023-05-07 15:51:51 +0200

    SC50001A3.2-030523App

--- client/App.js
+++ client/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "./theme";
-import MainRouter from "./MainRouter"
-import {hot} from "react-hot-loader"
+import MainRouter from "./MainRouter";
+import { hot } from "react-hot-loader";
 
 const App = () => {
+  // Once the code has been rendered on the server side and it gets to the browser, remove the
+  //server side injected css
+  React.useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  });
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
